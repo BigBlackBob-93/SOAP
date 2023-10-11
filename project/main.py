b7059ed13a2aeb2e80a49b5cc1984b023f7e39fa
BigBlackBob-93 <101857203+BigBlackBob-93@users.noreply.github.com>
2023-10-12 02:52:35 +0700

    Flask integration

--- project/main.py
+++ project/main.py
@@ -1,9 +1,29 @@
-from project.temp_convert import CelsiusToFahrenheit, FahrenheitToCelsius
+from project.temp_convert import TempConvert
+from flask import (
+    Flask,
+    render_template,
+    request,
+)
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    if request.method == 'POST':
+        temp: str = request.form['temp']
+        conv: TempConvert = TempConvert.get_conversion(unit=request.form['question'])
+        result: str | None = conv.parse(
+            data=conv.convert(
+                value=temp,
+            ),
+        )
+        return render_template(
+            'index.html',
+            result=result,
+        )
+    return render_template('index.html')
+
 
 if __name__ == '__main__':
-    conv = CelsiusToFahrenheit()
-    print(
-        conv.parse(
-            conv.convert('20')
-        )
-    )
+    app.run(debug=True)
--- project/temp_convert.py
+++ project/temp_convert.py
@@ -1,20 +1,34 @@
 from abc import ABC, abstractmethod
 import requests
 import xml.etree.ElementTree as ET
+from typing import Any
 
 
 class TempConvert(ABC):
     """Base abstract conversion class, defines behavior.
 
-    Fields: url: str | options: dict.
-    Methods: convert() -> Response | parse() -> str
+    Class fields: conversations: None|dict.
+    Class methods: get_conversion() -> Any.
+    Fields: url: str, options: dict.
+    Methods: convert() -> Response, parse() -> str
     """
+    conversions: dict[str, Any] = None
 
-    def __init__(self):
+    def __init__(self, unit: str) -> None:
+        self.unit: str = unit
         self.url: str = 'https://www.w3schools.com/xml/tempconvert.asmx'
         self.options: dict = {
             'Content-Type': 'text/xml; charset=utf-8',
         }
+
+    @classmethod
+    def get_conversion(cls, unit: str) -> Any:
+        if cls.conversions is None:
+            cls.conversions = {}
+            for conversion_class in cls.__subclasses__():
+                conversion: TempConvert = conversion_class()
+                cls.conversions[conversion.unit] = conversion
+        return cls.conversions[unit]
 
     @abstractmethod
     def convert(self, value: str) -> requests.Response:
@@ -30,8 +44,8 @@ class TempConvert(ABC):
 
 
 class CelsiusToFahrenheit(TempConvert):
-    def __init__(self):
-        super().__init__()
+    def __init__(self) -> None:
+        super().__init__('celsius')
 
     def convert(self, value: str) -> requests.Response:
         """Overridden function. Send a request to API with the created envelope, return a response."""
@@ -54,11 +68,12 @@ class CelsiusToFahrenheit(TempConvert):
 
 
 class FahrenheitToCelsius(TempConvert):
-    def __init__(self):
-        super().__init__()
+    def __init__(self) -> None:
+        super().__init__('fahrenheit')
 
     def convert(self, value: str) -> requests.Response:
         """Overridden function. Send a request to API with the created envelope, return a response."""
+
         envelope = f"""
         <soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
           <soap:Body>
@@ -72,4 +87,5 @@ class FahrenheitToCelsius(TempConvert):
 
     def parse(self, data: requests.Response) -> str | None:
         """Overridden function. Forward this method to the TempConvert."""
+
         return super().parse(data)
